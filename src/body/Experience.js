f86f1e1ca213f34bb7425934478fc98c3867bbe2
Yu Fu <kevin.yu.fu@gmail.com>
2026-05-12 17:02:51 -0400

    updates

--- src/body/Experience.js
+++ src/body/Experience.js
@@ -2,7 +2,7 @@ import '../css/experience.css';
 
 function Experience() {
   const exp = [
-    { company: 'Parallel Studios', position: 'Engineer', time: 'April 2024 – April 2025', points:
+    { company: 'Parallel Studios', position: 'Engineer', time: 'April 2024 – June 2025', points:
       [
         'Software Engineer for an AI driven simulation game. (Typescript, postgresql, HTMX, Tailwind)',
         'Increased control and reliability of character behavior by separating AI engines to handle dedicated tasks.',
@@ -19,12 +19,30 @@ function Experience() {
         'Led a rapid prototyping initiative developing 4 games in 4 weeks. Authored game design docs, strategized daily targets, delegated tasks to artists and coordinated with engineers through UML diagrams.',
       ]
     },
+    /*
     { company: 'Oracle', position: 'Backend Engineer', time: 'Jul 2019 – Jan 2020', points:
       [
         'Software Engineer on B2B advertising infrastructure team, working on database investigation and optimizations. (SQL, C#)',
         'Spearheaded automation testing initiative, completing over 120 missing tests and decreasing manual testing time by a week. (C#, Velocity)'
       ]
     },
+    */
+    { company: 'Rivals', position: 'CEO', time: 'Dec 2025 - Present', points:
+      [
+        //"Founded and launched an event company, successfully producing and executing its first large-scale event.",
+        "Founder of Rivals, a gaming rave which combining esports competitions with high energy music to build a new party experience for gamers.",
+        "Managed a team of 26, coordinating event planners, graphics designers, volunteers, and a media team.",
+        "Attended by 230+ guests, receiving overwhelmingly positive feedback and strong demand for future events.",
+      ]
+    },
+    { company: 'JSTDRMN', position: 'Academy Lead', time: 'Oct 2025 - Present', points:
+      [
+        "Lead the academy division of an artist incubator, designing and delivering result driven music production courses that help students build release ready tracks and performance skills."
+        //"The Academy Lead at an Artist Incubator Company.",
+        //"Designed and taught impactful and highly interactive courses, resulting in students playing at shows or releasing a track.",
+      ]
+    },
+    /*
     { company: 'Uber Maps', position: 'Backend Engineer', time: 'Jan 2018 – April 2018', points:
       [
         'Software Engineer on maps team, investigating graph partitioning algorithms, optimizing query speed by 18% and reducing storage by 10%. (Java, Python)'
@@ -37,6 +55,7 @@ function Experience() {
         'Led executive meetings for termly planning, event planning, and constitutional changes.'
       ]
     },
+    */
   ]
 
   const expElement = exp.map(x =>
--- src/body/Skills.js
+++ src/body/Skills.js
@@ -10,14 +10,18 @@ function Skills() {
       <h1>Skills</h1>
       <div>
         <ul>
-        {languages.map( x => (
-          <li><i>{x}</i></li>
+        {languages.map( (x, i) => (
+          <li><i>
+            {x}{i !== languages.length - 1 && ' | '}
+          </i></li>
         ))}
         </ul>
 
         <ul>
-        {tools.map( x => (
-          <li><i>{x}</i></li>
+        {tools.map( (x, i) => (
+          <li><i>
+            {x}{i !== tools.length - 1 && ' | '}
+          </i></li>
         ))}
         </ul>
       </div>
